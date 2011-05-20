c54601bdcde3abf5cd8b8f852e84c1929a74527f
Jacques Kvam <jwkvam@gmail.com>
2011-05-19 23:37:41 -0700

    added some comments

--- MyBot.hs
+++ MyBot.hs
@@ -18,15 +18,24 @@ generateOrders a = map (Order a) [North .. West]
 {- | 
  - Implement this function to create orders.
  - It uses the IO Monad so algorithms can call timeRemaining.
+ -
+ - GameParams data holds values that are constant throughout the game
+ - GameState holds data that changes between each turn
+ - for each see Ants module for more information
  -}
 doTurn :: GameParams -> GameState -> IO [Order]
 doTurn gp gs = do
+  -- generate orders for all ants belonging to me
   let generatedOrders = map generateOrders $ myAnts $ ants gs
+  -- for each ant take the first "passable" order, if one exists
       orders = mapMaybe (tryOrder (world gs)) generatedOrders
+  -- this shows how to check the remaining time
   elapsedTime <- timeRemaining gs
   hPutStrLn stderr $ show elapsedTime
+  -- wrap list of orders back into a monad
   return orders
 
+-- | This runs the game
 main :: IO ()
 main = game doTurn
 
